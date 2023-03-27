fad87df55d6c4632a0f1a16e4c406dba24c7ab7d
Smitesh Patil <smiteshpatil2010@gmail.com>
2023-03-27 08:00:56 +0530

    fix: Don't auto attach to lsp without docSymbols

--- lua/nvim-navbuddy/init.lua
+++ lua/nvim-navbuddy/init.lua
@@ -308,6 +308,9 @@ function M.setup(user_config)
 			callback = function(args)
 				local bufnr = args.buf
 				local client = vim.lsp.get_client_by_id(args.data.client_id)
+				if not client.server_capabilities.documentSymbolProvider then
+					return
+				end
 				M.attach(client, bufnr)
 			end,
 		})
