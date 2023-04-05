824fe2b56add158b8603def68f07f6bb405f543b
Smitesh Patil <smiteshpatil2010@gmail.com>
2023-04-05 21:30:38 +0530

    refactor: Move highlight setup to ui file

--- lua/nvim-navbuddy/display.lua
+++ lua/nvim-navbuddy/display.lua
@@ -8,66 +8,6 @@ local ui = require("nvim-navbuddy.ui")
 
 local ns = vim.api.nvim_create_namespace("nvim-navbuddy")
 
-local function highlight_setup()
-	for lsp_num = 1, 26 do
-		local navbuddy_ok, _ =
-			pcall(vim.api.nvim_get_hl_by_name, "Navbuddy" .. navic.adapt_lsp_num_to_str(lsp_num), false)
-		local navic_ok, navic_hl =
-			pcall(vim.api.nvim_get_hl_by_name, "NavicIcons" .. navic.adapt_lsp_num_to_str(lsp_num), true)
-
-		if not navbuddy_ok and navic_ok then
-			navic_hl = navic_hl["foreground"]
-
-			vim.api.nvim_set_hl(0, "Navbuddy" .. navic.adapt_lsp_num_to_str(lsp_num), {
-				fg = navic_hl,
-			})
-		end
-
-		local ok, navbuddy_hl =
-			pcall(vim.api.nvim_get_hl_by_name, "Navbuddy" .. navic.adapt_lsp_num_to_str(lsp_num), true)
-		if ok then
-			navbuddy_hl = navbuddy_hl["foreground"]
-			vim.api.nvim_set_hl(0, "NavbuddyCursorLine" .. navic.adapt_lsp_num_to_str(lsp_num), { bg = navbuddy_hl })
-		else
-			local _, normal_hl = pcall(vim.api.nvim_get_hl_by_name, "Normal", true)
-			normal_hl = normal_hl["foreground"]
-			vim.api.nvim_set_hl(0, "Navbuddy" .. navic.adapt_lsp_num_to_str(lsp_num), { fg = normal_hl })
-			vim.api.nvim_set_hl(0, "NavbuddyCursorLine" .. navic.adapt_lsp_num_to_str(lsp_num), { bg = normal_hl })
-		end
-	end
-
-	local ok, _ = pcall(vim.api.nvim_get_hl_by_name, "NavbuddyCursorLine", false)
-	if not ok then
-		vim.api.nvim_set_hl(0, "NavbuddyCursorLine", {
-			reverse = true,
-			bold = true,
-		})
-	end
-
-	ok, _ = pcall(vim.api.nvim_get_hl_by_name, "NavbuddyCursor", false)
-	if not ok then
-		vim.api.nvim_set_hl(0, "NavbuddyCursor", {
-			bg = "#000000",
-			blend = 100,
-		})
-	end
-
-	ok, _ = pcall(vim.api.nvim_get_hl_by_name, "NavbuddyName", false)
-	if not ok then
-		vim.api.nvim_set_hl(0, "NavbuddyName", { link = "IncSearch" })
-	end
-
-	ok, _ = pcall(vim.api.nvim_get_hl_by_name, "NavbuddyScope", false)
-	if not ok then
-		vim.api.nvim_set_hl(0, "NavbuddyScope", { link = "Visual" })
-	end
-
-	ok, _ = pcall(vim.api.nvim_get_hl_by_name, "NavbuddyFloatBorder", false)
-	if not ok then
-		vim.api.nvim_set_hl(0, "NavbuddyFloatBorder", { link = "FloatBorder" })
-	end
-end
-
 local function clear_buffer(buf)
 	vim.api.nvim_buf_set_option(buf.bufnr, "modifiable", true)
 	vim.api.nvim_buf_set_lines(buf.bufnr, 0, -1, false, {})
@@ -117,7 +57,7 @@ end
 local display = {}
 
 function display:new(obj)
-	highlight_setup()
+	ui.highlight_setup()
 
 	-- Object
 	setmetatable(obj, self)
--- lua/nvim-navbuddy/ui.lua
+++ lua/nvim-navbuddy/ui.lua
@@ -1,3 +1,5 @@
+local navic = require("nvim-navic.lib")
+
 local ui = {}
 
 function ui.get_border_chars(style, section)
@@ -107,6 +109,66 @@ function ui.get_border_chars(style, section)
 		},
 	}
 	return border_chars_map[section]
+end
+
+function ui.highlight_setup()
+	for lsp_num = 1, 26 do
+		local navbuddy_ok, _ =
+			pcall(vim.api.nvim_get_hl_by_name, "Navbuddy" .. navic.adapt_lsp_num_to_str(lsp_num), false)
+		local navic_ok, navic_hl =
+			pcall(vim.api.nvim_get_hl_by_name, "NavicIcons" .. navic.adapt_lsp_num_to_str(lsp_num), true)
+
+		if not navbuddy_ok and navic_ok then
+			navic_hl = navic_hl["foreground"]
+
+			vim.api.nvim_set_hl(0, "Navbuddy" .. navic.adapt_lsp_num_to_str(lsp_num), {
+				fg = navic_hl,
+			})
+		end
+
+		local ok, navbuddy_hl =
+			pcall(vim.api.nvim_get_hl_by_name, "Navbuddy" .. navic.adapt_lsp_num_to_str(lsp_num), true)
+		if ok then
+			navbuddy_hl = navbuddy_hl["foreground"]
+			vim.api.nvim_set_hl(0, "NavbuddyCursorLine" .. navic.adapt_lsp_num_to_str(lsp_num), { bg = navbuddy_hl })
+		else
+			local _, normal_hl = pcall(vim.api.nvim_get_hl_by_name, "Normal", true)
+			normal_hl = normal_hl["foreground"]
+			vim.api.nvim_set_hl(0, "Navbuddy" .. navic.adapt_lsp_num_to_str(lsp_num), { fg = normal_hl })
+			vim.api.nvim_set_hl(0, "NavbuddyCursorLine" .. navic.adapt_lsp_num_to_str(lsp_num), { bg = normal_hl })
+		end
+	end
+
+	local ok, _ = pcall(vim.api.nvim_get_hl_by_name, "NavbuddyCursorLine", false)
+	if not ok then
+		vim.api.nvim_set_hl(0, "NavbuddyCursorLine", {
+			reverse = true,
+			bold = true,
+		})
+	end
+
+	ok, _ = pcall(vim.api.nvim_get_hl_by_name, "NavbuddyCursor", false)
+	if not ok then
+		vim.api.nvim_set_hl(0, "NavbuddyCursor", {
+			bg = "#000000",
+			blend = 100,
+		})
+	end
+
+	ok, _ = pcall(vim.api.nvim_get_hl_by_name, "NavbuddyName", false)
+	if not ok then
+		vim.api.nvim_set_hl(0, "NavbuddyName", { link = "IncSearch" })
+	end
+
+	ok, _ = pcall(vim.api.nvim_get_hl_by_name, "NavbuddyScope", false)
+	if not ok then
+		vim.api.nvim_set_hl(0, "NavbuddyScope", { link = "Visual" })
+	end
+
+	ok, _ = pcall(vim.api.nvim_get_hl_by_name, "NavbuddyFloatBorder", false)
+	if not ok then
+		vim.api.nvim_set_hl(0, "NavbuddyFloatBorder", { link = "FloatBorder" })
+	end
 end
 
 return ui
